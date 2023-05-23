fd5b2075637847dcaa71b96aaa2b10dcd8b44201
csjhonathan <jhonathancarv.s@gmail.com>
2023-05-23 13:10:38 -0300

    fix: style from ranking list

--- src/styles/rankingPageStyles.js
+++ src/styles/rankingPageStyles.js
@@ -13,6 +13,19 @@ export const HomePageContainer = styled.div`
 export const List = styled.ul`
   display: flex;
   flex-direction: column;
+  box-sizing: border-box;
+  width: 1017px;
+  background: #FFFFFF;
+  border: 1px solid rgba(120, 177, 89, 0.25);
+  box-shadow: 0px 4px 24px rgba(120, 177, 89, 0.12);
+  border-radius: 24px 24px 0px 0px;
+  padding-left:40px ;
+  padding-bottom: 30px;
+  margin-top: 57px;
+  overflow-y: scroll;
+  &::-webkit-scrollbar{
+    
+  }
 `;
 
 export const LinkItem = styled.li`
@@ -21,6 +34,7 @@ export const LinkItem = styled.li`
   font-size: 22px;
   line-height: 28px;
   color: #000000;
+  margin-top: 19px;
 `;
 
 export const RankingLogo = styled.div`
